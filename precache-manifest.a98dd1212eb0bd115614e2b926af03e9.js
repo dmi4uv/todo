82f00284603b58b37cf425ffe9c49aec07a6f47d
Chuvykin Dmitry <dmi4uv@gmail.com>
2020-10-26 23:07:02 +0400

    Updates

---
+++ precache-manifest.a98dd1212eb0bd115614e2b926af03e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c949eddac12e17d3565c9564fe0469ad",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "66af51623aa515693edf",
+    "url": "/todo/static/css/main.ce81c6bc.chunk.css"
+  },
+  {
+    "revision": "05179f275fdea949f9c2",
+    "url": "/todo/static/js/2.84f7c15e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo/static/js/2.84f7c15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66af51623aa515693edf",
+    "url": "/todo/static/js/main.07fd1afe.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
